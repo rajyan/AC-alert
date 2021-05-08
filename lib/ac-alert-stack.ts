de4c78159a9f2c28f2a65a83207f9f5cbf2ad904
rajyan <kitakita7617@gmail.com>
2021-05-09 00:16:03 +0900

    create infra

--- lib/ac-alert-stack.ts
+++ lib/ac-alert-stack.ts
@@ -1,9 +1,45 @@
 import * as cdk from '@aws-cdk/core';
+import * as s3 from '@aws-cdk/aws-s3';
+import * as lambda from '@aws-cdk/aws-lambda';
+import * as events from '@aws-cdk/aws-events';
+import * as targets from '@aws-cdk/aws-events-targets';
 
 export class AcAlertStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const bucket = new s3.Bucket(this, 'LastACMemo', {
+      bucketName: 'last-ac-memo',
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true
+    });
+
+    const func = new lambda.Function(this, 'Checker', {
+      runtime: lambda.Runtime.NODEJS_14_X,
+      code: lambda.Code.fromAsset('src'),
+      handler: 'ac-checker.main',
+      environment: {
+        BUCKET_NAME: bucket.bucketName
+      }
+    });
+    bucket.grantReadWrite(func);
+
+    const rule1 = new events.Rule(this, 'Rule1',{
+      ruleName: 'rule on 10pm',
+      schedule: events.Schedule.cron({
+        minute: '0/30',
+        hour: '22'
+      }),
+      targets: [new targets.LambdaFunction(func)]
+    });
+    const rule2 = new events.Rule(this, 'Rule2',{
+      ruleName: 'rule on 11pm',
+      schedule: events.Schedule.cron({
+        minute: '0/11',
+        hour: '23'
+      }),
+      targets: [new targets.LambdaFunction(func)]
+    });
+
   }
 }
