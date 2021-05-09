87051cbbd9aab8b8784e71140c0d3f4fda41657b
rajyan <kitakita7617@gmail.com>
2021-05-09 12:12:07 +0900

    delete comment

--- src/ac-checker.ts
+++ src/ac-checker.ts
@@ -1,13 +1,3 @@
-/*
-This code uses callbacks to handle asynchronous function responses.
-It currently demonstrates using an async-await pattern.
-AWS supports both the async-await and promises patterns.
-For more information, see the following:
-https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/async_function
-https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Using_promises
-https://docs.aws.amazon.com/sdk-for-javascript/v2/developer-guide/calling-services-asynchronously.html
-https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-handler.html
-*/
 import {S3} from 'aws-sdk';
 import axios from 'axios';
 
